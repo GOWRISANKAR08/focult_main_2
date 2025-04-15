88bf5fd4c8ee4e329d250d5ee066b7a91441fafd
GOWRISANKAR08 <sankar08garu@gmail.com>
2025-04-15 23:43:50 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="user_module",
+    name="user_module",  # Must match import name exactly
     version="0.1.0",
-    packages=find_packages(),
+    packages=["user_module"],
+    package_dir={"": "."},
     include_package_data=True,
     install_requires=["Django>=5.0"],
-    python_requires=">=3.8",
 )
