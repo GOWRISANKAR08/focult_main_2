a727a3cd46deecfc35d8b46018883f85afc6cf33
GOWRISANKAR08 <sankar08garu@gmail.com>
2025-04-25 10:49:24 +0530

    project_structure

--- setup.py
+++ setup.py
@@ -26,29 +26,29 @@
 
 # from setuptools import setup, find_packages
 
-# from setuptools import setup, find_packages
-
-# setup(
-#     name="main_app",
-#     version="0.1.0",
-#     packages=['main_app', 'main_app.user_module', 'main_app.order_module'],  # Explicit list
-#     package_dir={'main_app': 'main_app'},  # Maps package name to directory
-#     include_package_data=True,
-#     install_requires=["Django>=5.0"],
-#     python_requires=">=3.8",
-#     zip_safe=False,
-# )
-
-
 from setuptools import setup, find_packages
 
 setup(
     name="main_app",
     version="0.1.0",
-    packages=find_packages(include=['main_app', 'main_app.*']),  # Explicit include
-    package_dir={'': '.'},  # Look in current directory
+    packages=['main_app', 'main_app.user_module', 'main_app.order_module'],  # Explicit list
+    package_dir={'main_app': 'main_app'},  # Maps package name to directory
     include_package_data=True,
     install_requires=["Django>=5.0"],
     python_requires=">=3.8",
     zip_safe=False,
 )
+
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="main_app",
+#     version="0.1.0",
+#     packages=find_packages(include=['main_app', 'main_app.*']),  # Explicit include
+#     package_dir={'': '.'},  # Look in current directory
+#     include_package_data=True,
+#     install_requires=["Django>=5.0"],
+#     python_requires=">=3.8",
+#     zip_safe=False,
+# )
